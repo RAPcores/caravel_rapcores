ed348194fd159e03a4c0370a831017337ecdc682
Steve Kelly <kd2cca@gmail.com>
2020-11-09 14:28:36 -0500

    init

---
+++ openlane.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+# build openlane
+mkdir -p pdk_root
+export PDK_ROOT=`pwd`/pdk_root
+git clone https://github.com/efabless/openlane.git --branch rc4
+cd openlane
+make
+cd docker_build
+make merge
+cd ../..
