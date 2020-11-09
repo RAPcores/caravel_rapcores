7daccb48eb995033efdc4eb2b8202da22446c39e
Steve Kelly <kd2cca@gmail.com>
2020-11-09 15:17:16 -0500

    export PDK_ROOT for caravel
    add clean
    
    fixes to build user_proj_example
    
    use local caravel
    
    prep

--- openlane.sh
+++ openlane.sh
@@ -3,7 +3,7 @@
 # build openlane
 mkdir -p pdk_root
 export PDK_ROOT=`pwd`/pdk_root
-git clone https://github.com/efabless/openlane.git --branch rc4
+git clone https://github.com/efabless/openlane.git --branch develop
 cd openlane
 make
 cd docker_build
