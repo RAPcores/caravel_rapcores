9f4bcb9316fb4bf886b55260842a67a2a4362939
johnnyr <johnny@ultimachine.com>
2020-11-29 17:48:54 -0600

    microstepper tcl

--- openlane/microstepper/config.tcl
+++ openlane/microstepper/config.tcl
@@ -6,20 +6,11 @@ set ::env(PDN_CFG) $script_dir/pdn.tcl
 
 set ::env(STD_CELL_LIBRARY) "sky130_fd_sc_hvl"
 
-set ::env(VERILOG_FILES) "\
-        $script_dir/../../rapcores/src/pwm.v \
-        $script_dir/../../rapcores/src/microstepper/microstepper_top.v \
-        $script_dir/../../rapcores/src/microstepper/microstep_counter.v \
-        $script_dir/../../rapcores/src/microstepper/cosine.v \
-        $script_dir/../../rapcores/src/microstepper/analog_out.v \
-        $script_dir/../../rapcores/src/microstepper/chargepump.v \
-        $script_dir/../../rapcores/src/microstepper/mytimer.v"
-
 set ::env(CLOCK_PORT) "clk"
 set ::env(CLOCK_PERIOD) "15"
 
 set ::env(FP_SIZING) absolute
-set ::env(DIE_AREA) "0 0 1000 1000"
+set ::env(DIE_AREA) "0 0 1500 1500"
 set ::env(DESIGN_IS_CORE) 0
 
 #set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
@@ -27,3 +18,22 @@ set ::env(DESIGN_IS_CORE) 0
 set ::env(PL_BASIC_PLACEMENT) 1
 set ::env(FP_CORE_UTIL) "40"
 set ::env(PL_TARGET_DENSITY) "0.42"
+
+set ::env(VERILOG_FILES) "\
+        $script_dir/../../rapcores/src/microstepper/microstepper_top.v \
+        $script_dir/../../rapcores/src/microstepper/microstep_counter.v \
+        $script_dir/../../rapcores/src/microstepper/cosine.v \
+        $script_dir/../../rapcores/src/microstepper/mytimer.v"
+
+set ::env(VERILOG_FILES_BLACKBOX) "\
+	$script_dir/../../rapcores/src/microstepper/analog_out.v \
+        $script_dir/../../rapcores/src/microstepper/chargepump.v"
+
+set ::env(EXTRA_LEFS) "\
+	$script_dir/../../lef/analog_out.lef \
+	$script_dir/../../lef/chargepump.lef"
+
+set ::env(EXTRA_GDS_FILES) "\
+	$script_dir/../../gds/analog_out.gds \
+	$script_dir/../../gds/chargepump.gds"
+
