38131a940c40f17fc9160254d2ed3b591a9acbbb
johnnyr <johnny@ultimachine.com>
2020-11-29 14:20:22 -0600

    prototype quad_enc script

---
+++ openlane/quad_encoder/config.tcl
@@ -0,0 +1,19 @@
+set script_dir [file dirname [file normalize [info script]]]
+
+set ::env(DESIGN_NAME) quad_enc
+
+set ::env(VERILOG_FILES) "\
+        $script_dir/../../rapcores/src/quad_enc.v"
+
+set ::env(CLOCK_PORT) "clk"
+set ::env(CLOCK_PERIOD) "15"
+
+set ::env(FP_SIZING) absolute
+set ::env(DIE_AREA) "0 0 200 200"
+set ::env(DESIGN_IS_CORE) 0
+
+#set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
+
+set ::env(PL_BASIC_PLACEMENT) 1
+set ::env(FP_CORE_UTIL) "50"
+set ::env(PL_TARGET_DENSITY) "0.52"
