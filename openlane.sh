048b6f72da1d7102d109d80035e8046417418a21
Steve Kelly <kd2cca@gmail.com>
2020-11-29 00:17:09 -0500

    new openlane location

--- openlane.sh
+++ openlane.sh
@@ -3,8 +3,8 @@
 # build openlane
 mkdir -p pdk_root
 export PDK_ROOT=`pwd`/pdk_root
-git clone https://github.com/efabless/openlane.git --branch develop
-cd openlane
+git clone https://github.com/efabless/openlane.git --branch develop openlane_repo
+cd openlane_repo
 make
 cd docker_build
 make merge
