ab514ec30ed6d6052738d273a8f4dca2b2d31dbc
Steve Kelly <kd2cca@gmail.com>
2020-11-30 12:54:55 -0500

    build from our own repo making it easier to patch

--- openlane.sh
+++ openlane.sh
@@ -3,7 +3,7 @@
 # build openlane
 mkdir -p pdk_root
 export PDK_ROOT=`pwd`/pdk_root
-git clone https://github.com/efabless/openlane.git --branch develop openlane_repo
+git clone https://github.com/RAPcores/openlane.git --branch rapcore_flow openlane_repo
 cd openlane_repo
 make
 cd docker_build
