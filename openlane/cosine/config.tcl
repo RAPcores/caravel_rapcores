19fdba747a74e7f7e9d7aea179646a46b293c2bc
johnnyr <johnny@ultimachine.com>
2020-11-30 19:42:39 -0600

    cosine config

--- openlane/cosine/config.tcl
+++ openlane/cosine/config.tcl
@@ -6,15 +6,15 @@ set ::env(DESIGN_NAME) cosine
 set ::env(VERILOG_FILES) "\
         $script_dir/../../rapcores/src/microstepper/cosine.v"
 
-set ::env(CLOCK_PORT) "clk"
-set ::env(CLOCK_PERIOD) "10"
+#set ::env(CLOCK_PORT) "clk"
+#set ::env(CLOCK_PERIOD) "10"
 
 set ::env(FP_SIZING) absolute
-set ::env(DIE_AREA) "0 0 50 100"
+set ::env(DIE_AREA) "0 0 100 100"
 set ::env(DESIGN_IS_CORE) 0
 
 #set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
 
 set ::env(PL_BASIC_PLACEMENT) 1
-set ::env(FP_CORE_UTIL) "50"
-set ::env(PL_TARGET_DENSITY) "0.55"
+#set ::env(FP_CORE_UTIL) "50"
+set ::env(PL_TARGET_DENSITY) "0.42"
