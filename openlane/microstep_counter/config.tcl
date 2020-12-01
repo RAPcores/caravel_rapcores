b97665d3742741df0d0d921cc2e3e2195df5496a
johnnyr <johnny@ultimachine.com>
2020-12-01 01:38:25 -0600

    work on macros

--- openlane/microstep_counter/config.tcl
+++ openlane/microstep_counter/config.tcl
@@ -1,18 +1,17 @@
 set script_dir [file dirname [file normalize [info script]]]
 
-set ::env(DESIGN_NAME) cosine
+set ::env(DESIGN_NAME) microstep_counter
 
 set ::env(VERILOG_FILES) "\
-        $script_dir/../../rapcores/src/microstepper/cosine.v"
+        $script_dir/../../rapcores/src/microstepper/microstep_counter.v"
 
 #set ::env(CLOCK_PORT) "clk"
 #set ::env(CLOCK_PERIOD) "10"
 
 set ::env(FP_SIZING) absolute
-set ::env(DIE_AREA) "0 0 100 100"
+set ::env(DIE_AREA) "0 0 50 40"
 set ::env(DESIGN_IS_CORE) 0
 set ::env(GLB_RT_MAXLAYER) 5
-
 #set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
 
 set ::env(PL_BASIC_PLACEMENT) 1
--- openlane/pwm/config.tcl
+++ openlane/pwm/config.tcl
@@ -1,18 +1,18 @@
 set script_dir [file dirname [file normalize [info script]]]
 
-set ::env(DESIGN_NAME) analog_out
+set ::env(DESIGN_NAME) PWM
 
 set ::env(VERILOG_FILES) "\
-        $script_dir/../../rapcores/src/microstepper/analog_out.v"
+        $script_dir/../../rapcores/src/pwm.v"
 
 set ::env(CLOCK_PORT) "clk"
 set ::env(CLOCK_PERIOD) "10"
 
 set ::env(FP_SIZING) absolute
-set ::env(DIE_AREA) "0 0 100 100"
+set ::env(DIE_AREA) "0 0 50 100"
 set ::env(DESIGN_IS_CORE) 0
-set ::env(FP_PDN_CORE_RING) 0
 set ::env(GLB_RT_MAXLAYER) 5
+set ::env(GLB_RT_MINLAYER) 2
 
 #set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
 
