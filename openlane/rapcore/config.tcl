bfcf2f35fc59a7a85711971a77f956a4bf083ff2
Steve Kelly <kd2cca@gmail.com>
2020-12-09 00:16:30 -0500

    update config for changes in rapcore, flow untested

--- openlane/rapcore/config.tcl
+++ openlane/rapcore/config.tcl
@@ -1,29 +1,33 @@
 set script_dir [file dirname [file normalize [info script]]]
 
-set ::env(DESIGN_NAME) top
+set ::env(DESIGN_NAME) rapcore
 
 set ::env(VERILOG_FILES) "\
         $script_dir/../../verilog/rtl/defines.v \
         $script_dir/../../rapcores/src/constants.v \
         $script_dir/../../rapcore_caravel_defines.v \
         $script_dir/../../rapcores/src/macro_params.v \
-        $script_dir/../../rapcores/src/top.v \
+        $script_dir/../../rapcores/src/spi_state_machine.v \
+        $script_dir/../../rapcores/src/dda_timer.v \
         $script_dir/../../rapcores/src/stepper.v \
         $script_dir/../../rapcores/src/spi.v \
         $script_dir/../../rapcores/src/quad_enc.v \
         $script_dir/../../rapcores/src/pwm.v \
+        $script_dir/../../rapcores/src/microstepper/microstepper_control.v \
         $script_dir/../../rapcores/src/microstepper/microstepper_top.v \
         $script_dir/../../rapcores/src/microstepper/microstep_counter.v \
         $script_dir/../../rapcores/src/microstepper/cosine.v \
         $script_dir/../../rapcores/src/microstepper/analog_out.v \
         $script_dir/../../rapcores/src/microstepper/chargepump.v \
-        $script_dir/../../rapcores/src/microstepper/mytimer.v"
-
+        $script_dir/../../rapcores/src/microstepper/mytimer.v \
+        $script_dir/../../rapcores/src/microstepper/mytimer_8.v \
+        $script_dir/../../rapcores/src/microstepper/mytimer_10.v \
+        $script_dir/../../rapcores/src/rapcore.v"
 set ::env(CLOCK_PORT) "CLK"
 set ::env(CLOCK_PERIOD) "15"
 
 set ::env(FP_SIZING) absolute
-set ::env(DIE_AREA) "0 0 540 540"
+set ::env(DIE_AREA) "0 0 500 500"
 
 # use the empty wrapper to help pin placement
 set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
