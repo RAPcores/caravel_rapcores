6ca5c8c57d289e219f9475a3c68f31ef1d9f3323
Steve Kelly <kd2cca@gmail.com>
2020-12-09 13:06:14 -0500

    up precheck

--- precheck.sh
+++ precheck.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env bash
 
-git clone git@github.com:efabless/open_mpw_precheck.git
-cd open_mpw_precheck/dependencies
-sh build-docker.sh
+cd ../open_mpw_precheck/dependencies
+#sh build-docker.sh
 cd ..
-export TARGET_PATH=`pwd`/..
+export TARGET_PATH=/home/steve/Ultimachine/caravel_rapcores2
+export PDK_ROOT=/home/steve/Ultimachine/openlane/pdk_root
 docker run -it -v $(pwd):/usr/local/bin \
     -u $(id -u $USER):$(id -g $USER) -v $TARGET_PATH:$TARGET_PATH \
-    open_mpw_prechecker:latest python3 open_mpw_prechecker.py -t $TARGET_PATH
+    open_mpw_prechecker:latest python3 open_mpw_prechecker.py -t $TARGET_PATH -p /home/steve/Ultimachine/openlane/pdk_root
