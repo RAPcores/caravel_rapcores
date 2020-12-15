cc7eeb11d41771b775aa328422106c7094f16238
johnnyr <johnny@ultimachine.com>
2020-12-15 17:01:45 -0600

    temp cv tb hack

--- verilog/rtl/user_project_wrapper.v
+++ verilog/rtl/user_project_wrapper.v
@@ -65,6 +65,8 @@ module user_project_wrapper #(
     /*--------------------------------------*/
 
     rapcores rapcores0 (
+
+`ifndef SIM
     `ifdef USE_POWER_PINS
 	.vdda1(vdda1),	// User area 1 3.3V power
 	.vdda2(vdda2),	// User area 2 3.3V power
@@ -75,7 +77,7 @@ module user_project_wrapper #(
 	.vssd1(vssd1),	// User area 1 digital ground
 	.vssd2(vssd2),	// User area 2 digital ground
     `endif
-
+`endif
 	// MGMT core clock and reset
 
     	.wb_clk_i(wb_clk_i),
