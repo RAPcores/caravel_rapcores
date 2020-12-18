7407b87af2489bcf2a6294327a31ef3064408395
Tonokip <tonokip@gmail.com>
2020-12-18 11:08:23 -0600

    fix pin map

--- verilog/rtl/rapcore_caravel.v
+++ verilog/rtl/rapcore_caravel.v
@@ -104,8 +104,8 @@ module rapcores #(
     assign io_oeb[18] = 1'b0;    // PHASE_A2_H
     assign io_oeb[14] = 1'b0;    // PHASE_B1_H
     assign io_oeb[17] = 1'b0;    // PHASE_B2_H
-    assign io_oeb[18] = 1'b1;    // ENC_B
-    assign io_oeb[19] = 1'b1;    // ENC_A
+    assign io_oeb[12] = 1'b1;    // ENC_B
+    assign io_oeb[13] = 1'b1;    // ENC_A
     assign io_oeb[12] = 1'b0;    // BUFFER_DTR
     assign io_oeb[24] = 1'b0;    // MOVE_DONE
     assign io_oeb[29] = 1'b1;    // HALT
@@ -126,7 +126,6 @@ module rapcores #(
     assign io_oeb[5] = 1'b1;    // Rx
     assign io_oeb[6] = 1'b1;    // Tx
     assign io_oeb[7] = 1'b1;    // IRQ
-    assign io_oeb[13] = 1'b1;   
     assign io_oeb[22] = 1'b1;
     assign io_oeb[34] = 1'b1;
     assign io_oeb[35] = 1'b1;
