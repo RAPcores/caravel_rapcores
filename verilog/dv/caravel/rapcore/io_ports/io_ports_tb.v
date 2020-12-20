56ccffc6caad9746d0fd68ec9fb23abd01da1fdb
Tonokip <tonokip@gmail.com>
2020-12-19 23:22:57 -0600

    refactor include structure for GL test.

--- verilog/dv/caravel/rapcore/io_ports/io_ports_tb.v
+++ verilog/dv/caravel/rapcore/io_ports/io_ports_tb.v
@@ -2,34 +2,34 @@
 
 `timescale 1 ns / 1 ps
 
-`include "defines.v"
-`include "mpw_one_defines.v"
-`include "macro_params.v"
-`include "constants.v"
-`include "quad_enc.v"
-`include "spi.v"
-`include "dda_timer.v"
-`include "spi_state_machine.v"
-`include "microstepper/chargepump.v"
-`include "microstepper/microstepper_control.v"
-`include "microstepper/mytimer_8.v"
-`include "microstepper/mytimer_10.v"
-`include "microstepper/microstep_counter.v"
-`include "microstepper/cosine.v"
-`include "microstepper/analog_out.v"
-`include "microstepper/microstepper_top.v"
-`include "rapcore.v"
+`ifdef PROJ_GL
+  `include "rapcores.lvs.powered.v"
+`else
+	`include "defines.v"
+	`include "mpw_one_defines.v"
+	`include "macro_params.v"
+	`include "constants.v"
+	`include "quad_enc.v"
+	`include "spi.v"
+	`include "dda_timer.v"
+	`include "spi_state_machine.v"
+	`include "microstepper/chargepump.v"
+	`include "microstepper/microstepper_control.v"
+	`include "microstepper/mytimer_8.v"
+	`include "microstepper/mytimer_10.v"
+	`include "microstepper/microstep_counter.v"
+	`include "microstepper/cosine.v"
+	`include "microstepper/analog_out.v"
+	`include "microstepper/microstepper_top.v"
+	`include "rapcore.v"
+	`include "rapcores.v"
+`endif
+
 `include "hbridge_coil.v"
 `include "pwm_duty.v"
 `include "rapcore_harness_tb.v"
 
 //`define USE_POWER_PINS
-
-`ifdef PROJ_GL
-  `include "rapcores.lvs.powered.v"
-`else
-  `include "rapcores.v"
-`endif
 
 `include "caravel.v"
 `include "spiflash.v"
